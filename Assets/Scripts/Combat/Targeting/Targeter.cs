41d11eb59694092a681ba3370caf383103ec492e
swordboys923 <tdonahue630@gmail.com>
2023-09-11 22:44:08 -0400

    --Up through lecture "Targeting"

--- Assets/Scripts/Combat/Targeting/Targeter.cs
+++ Assets/Scripts/Combat/Targeting/Targeter.cs
@@ -6,12 +6,12 @@ public class Targeter : MonoBehaviour {
     private List<Target> targetList = new List<Target>();
 
     private void OnTriggerEnter(Collider other) {
-        // if(other.TryGetComponent<Target>()) {
-            
-        // }
+        if(!other.TryGetComponent<Target>(out Target target))return;
+        targetList.Add(target);
     }
 
     private void OnTriggerExit(Collider other) {
-        
+        if(!other.TryGetComponent<Target>(out Target target)) return;
+        targetList.Remove(target);
     }
 }
